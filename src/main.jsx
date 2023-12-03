443dbceeea67e0724d19782f53348653fdf37918
Alessandra Scarpellini <scaalessandra@icloud.com>
2023-12-03 21:08:52 +0100

    add Wrapper preparing student context

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AuthProviderWrapper } from "./context/auth.context";
+import { StudentProviderWrapper } from "./context/student.context";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
@@ -9,7 +10,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
     <AuthProviderWrapper>
-      <App />
+      <StudentProviderWrapper>
+        <App />
+      </StudentProviderWrapper>
     </AuthProviderWrapper>
   </Router>
 );
