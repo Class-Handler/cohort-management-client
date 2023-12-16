f093702dc089aaeb9ffd96f4ff586ffe887d279e
Alessandra Scarpellini <scaalessandra@icloud.com>
2023-12-16 17:29:41 +0100

    create student context

---
+++ src/context/student.context.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import studentService from "../services/student.services";
+const StudentContext = React.createContext();
+
+
+function StudentProviderWrapper(props) {
+  const [isValidate, setIsValidate] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [studentData, setStudentData] = useState(null);
+
+  const storeStudentToken = (token) => {
+    localStorage.setItem("studentToken", token);
+  };
+
+  const authenticateStudent = async () => {
+    const storedStudentToken = localStorage.getItem("studentToken");
+
+    if (storedStudentToken){
+      try{
+        const verifyed = await studentService.verifyStudent()
+        setStudentData(verifyed.data)
+        setIsValidate(true)
+        setIsLoading(false)
+      } catch(err){
+        setIsValidate(false)
+        setIsLoading(false)
+        setStudentData(null)
+      }
+    } else {
+      setIsValidate(false)
+        setIsLoading(false)
+        setStudentData(null)
+    }
+  };
+
+  const removeStudentToken = () => {
+    localStorage.removeItem("studentToken");
+  };
+
+  const logOutStudent = () => {
+    removeStudentToken();
+    authenticateStudent();
+  };
+
+  useEffect(() => {
+    authenticateStudent();
+  }, []);
+
+  return (
+    <StudentContext.Provider
+      value={{
+        isValidate,
+        isLoading,
+        studentData,
+        storeStudentToken,
+        authenticateStudent,
+        logOutStudent,
+      }}
+    >
+      {props.children}
+    </StudentContext.Provider>
+  );
+}
+
+export { StudentProviderWrapper, StudentContext };
