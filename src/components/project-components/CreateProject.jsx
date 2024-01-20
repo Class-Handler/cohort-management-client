32e90d6bd13a42670cfc58e2650a63b623476ad4
Alessandra Scarpellini <scaalessandra@icloud.com>
2024-01-21 00:59:32 +0100

    add possibility to select manually students to include as partecipants for the project

--- src/components/project-components/CreateProject.jsx
+++ src/components/project-components/CreateProject.jsx
@@ -1,17 +1,16 @@
 import { useState } from "react";
 import projectService from "../../services/project.services";
 
-const CreateProject = ({ cohortId, getCohort, partecipants }) => {
+const CreateProject = ({ cohortId, getCohort, getProject, allStudents }) => {
   const [project, setProject] = useState({
-    projectType: "Project 2",
+    projectType: null,
     preferencesNumber: 8,
     blockedNumber: 3,
-    partecipants: partecipants,
-    cohortId: cohortId
+    partecipants: allStudents.map(el => el._id),
+    cohortId: cohortId,
   });
+  const [togglePartecipants, setTogglePartecipants] = useState(false);
   const [errorMessage, setErrorMessage] = useState(undefined);
-
-  /* // projectSettings {} for req.body --> {projectType, preferencesNumber, blockedNumber} */
 
   const handleChange = (e) => {
     const name = e.target.name;
@@ -19,70 +18,94 @@ const CreateProject = ({ cohortId, getCohort, partecipants }) => {
     setProject((values) => ({ ...values, [name]: value }));
   };
 
+  const handlePartecipants = (e) => {
+    if (e.target.checked) {
+      const selectedPartecipantsList = [...project.partecipants, e.target.value];
+      setProject((prev) => ({...prev, partecipants: selectedPartecipantsList}));
+    } else {
+      const selectedPartecipantsList = project.partecipants.filter((el) => el !== e.target.value);
+      setProject((prev) => ({...prev, partecipants: selectedPartecipantsList}));
+    }
+  };
+
   const createProject = async (e) => {
     e.preventDefault();
-    console.log("PROJ ", project);
+
     try {
       const response = await projectService.createProject(cohortId, project);
-      alert("Project created");
-      console.log(response.data);
+      alert("Project created"); // needs to be changed
       getCohort();
+      getProject(response.data._id)
     } catch (err) {
-      console.log(err);
       setErrorMessage(err.response.data.message);
     }
   };
 
   return (
-    <div className="CreateProject">
-      <form onSubmit={createProject}>
-        <div className="mb-3">
+    <div className="CreateProject card p-2">
+      <form onSubmit={createProject} className="form">
+        <div className="d-flex justify-content-evenly mb-3 ">
           <div className="form-check m-1">
             <label className="form-check-label m-1">
               Project 2
               <input
-                className="form-check-input m-2"
+                className="form-check-input m-1"
                 type="radio"
                 name="projectType"
                 value="Project 2"
                 onChange={handleChange}
+                required
               />
             </label>
             <label className="form-check-label m-1">
               Final Project
               <input
-                className="form-check-input m-2"
+                className="form-check-input m-1"
                 type="radio"
                 name="projectType"
                 value="Final Project"
                 onChange={handleChange}
+                required
+              />
+            </label>
+            <label className="form-check-label m-1">
+              Mini Project Mod 1
+              <input
+                className="form-check-input m-1"
+                type="radio"
+                name="projectType"
+                value="Mini Project Mod 1"
+                onChange={handleChange}
+                required
               />
             </label>
             <label className="form-check-label m-1">
               Mini Project Mod 2
               <input
-                className="form-check-input  m-2"
+                className="form-check-input m-1"
                 type="radio"
                 name="projectType"
                 value="Mini Project Mod 2"
                 onChange={handleChange}
+                required
               />
             </label>
             <label className="form-check-label m-1">
               Mini Project Mod 3
               <input
-                className="form-check-input  m-2"
+                className="form-check-input m-1"
                 type="radio"
                 name="projectType"
                 value="Mini Project Mod 3"
                 onChange={handleChange}
+                required
               />
             </label>
           </div>
         </div>
 
-        <div className="mb-3">
-          <label className="form-label">Number of preferences (5-9):</label>
+        <div className="mb-3 d-flex justify-content-center">
+          <label className="form-label">Number of preferences (5-9):
           <input
             type="number"
             name="preferencesNumber"
@@ -93,60 +116,102 @@ const CreateProject = ({ cohortId, getCohort, partecipants }) => {
             max={9}
             required
           />
+          </label>
         </div>
 
-        <div className="mb-3">
+        <div className="mb-3 d-flex justify-content-center">
           <label className="form-label">
             Number of Not preferences (max 4):
-          </label>
           <input
             type="number"
             name="blockedNumber"
             value={project.blockedNumber}
             onChange={handleChange}
             className="form-control"
+            min={0}
             max={4}
             required
           />
+          </label>
         </div>
 
-          <div className="dropdown mb-3">
-            <button
-              className="btn btn-secondary dropdown-toggle"
-              type="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
+        <div className="mb-4 d-flex justify-content-center">
+          <label className="form-label me-3">Partecipants:</label>
+
+          {!togglePartecipants && (
+            <div
+              onChange={() => {
+                setTogglePartecipants(!togglePartecipants);
+                setProject((prev) => ({ ...prev, partecipants: [] }));
+              }}
             >
-              Select partecipants
-            </button>
-            {/* <ul className="dropdown-menu"> */}
-              {project.partecipants.map((student) => {
+              <div className="form-check">
+                <label className="form-check-label text-uppercase">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    defaultChecked
+                  />
+                  All students
+                </label>
+              </div>
+
+              <div className="form-check">
+                <label className="form-check-label">
+                  <input className="form-check-input" type="checkbox" />
+                  Select manually
+                </label>
+              </div>
+            </div>
+          )}
+
+          {togglePartecipants && (
+            <ul className="list-unstyled">
+              <li className="form-check">
+                <label className="form-check-label text-uppercase">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    onChange={(e) => {
+                      setTogglePartecipants(!togglePartecipants);
+                      setProject((prev) => ({
+                        ...prev,
+                        partecipants: allStudents.map((el) => el._id),
+                      }));
+                    }}
+                  />
+                  All students
+                </label>
+              </li>
+              {allStudents.map((student) => {
                 return (
-                  <li className="dropdown-item" key={student._id}>
-                    <div className="form-check">
+                  <li className="text-capitalize form-check" key={student._id}>
                     <label className="form-check-label">
                       <input
                         className="form-check-input"
                         type="checkbox"
-                        value={student.studentName}
+                        value={student._id}
+                        onChange={handlePartecipants}
                       />
-                      
-                        {student.studentName}
-                      </label>
-                    </div>
+
+                      {student.studentName}
+                    </label>
                   </li>
                 );
               })}
-            {/* </ul> */}
-          </div>
+            </ul>
+          )}
+        </div>
 
         {errorMessage && (
           <p className="error-message text-uppercase">- {errorMessage} -</p>
         )}
 
-        <button type="submit" className="btn btn-dark">
-          Submit
-        </button>
+        <div className="d-flex justify-content-center">
+          <button type="submit" className="btn btn-dark">
+            Submit
+          </button>
+        </div>
       </form>
     </div>
   );
