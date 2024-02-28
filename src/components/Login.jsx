a132fb2a8859c0b19c6c3b0e7569e6001b098f02
Alessandra Scarpellini <scaalessandra@icloud.com>
2024-02-28 19:47:52 +0100

    style with react-bootstrap components, add error handler

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,69 +3,70 @@ import authService from "../services/auth.services";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
 
-function Login() {
+import Button from "react-bootstrap/Button";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+
+import eyeIcon from "../assets/icon/eye.png";
+import hiddenEyeIcon from "../assets/icon/hidden-eye.png";
+
+function Login({ showAlert }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState(undefined);
+  const [showPassword, setShowPassword] = useState(false);
 
   const navigate = useNavigate();
 
   const { storeToken, authenticateUser } = useContext(AuthContext);
 
-  const handleLoginSubmit = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
-
-    authService.login({ email, password }) 
-      .then((response) => {
-        storeToken(response.data.authToken);
-        authenticateUser();
-        navigate("/my-cohorts");
-      })
-      .catch((error) => {
-        const errorDescription = error.response.data.message;
-        setErrorMessage(errorDescription);
-      });
+    try {
+      const response = await authService.login({ email, password });
+      storeToken(response.data.authToken);
+      authenticateUser();
+      navigate("/cohorts");
+    } catch (error) {
+      showAlert(`Oooops! ${error.response.data.message}`, "danger");
+    }
   };
 
   return (
-    <>
-          <form onSubmit={handleLoginSubmit} className="form">
-            <div className="form-outline mb-2">
-              <label className="form-label">
-                Email
-                <input
-                  className="form-control mt-2"
-                  type="email"
-                  name="email"
-                  value={email}
-                  required
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </label>
-            </div>
+    <Form onSubmit={handleLogin}>
+      <FloatingLabel label="Email" className="mb-3">
+        <Form.Control
+          type="email"
+          required
+          value={email}
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FloatingLabel>
 
-            <div className="form-outline mb-2">
-              <label className="form-label">
-                Password
-                <input
-                  className="form-control mt-2"
-                  type="password"
-                  name="password"
-                  value={password}
-                  required
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </label>
-            </div>
-            <button type="submit" className="btn btn-warning mb-3">
-              Login
-            </button>
-          </form>
+      <FloatingLabel label="Password" className="mb-3 w-80">
+        <Form.Control
+          type={showPassword ? "text" : "password"}
+          value={password}
+          placeholder="Password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </FloatingLabel>
 
-          {errorMessage && (
-            <p className="error-message text-uppercase">- {errorMessage} -</p>
-          )}
-        </>
+      <Button type="submit" variant="dark" className="me-3">
+        Submit
+      </Button>
+      <Button
+        variant="outline-light"
+        onClick={() => setShowPassword(!showPassword)}
+      >
+        <img
+          src={!showPassword ? eyeIcon : hiddenEyeIcon}
+          alt="show - hide"
+          width={"30px"}
+        />
+      </Button>
+    </Form>
   );
 }
 
